6fea0fd317a6fd8f1d9869d176b0a3c208e59961
MarcosMoraes23 <marcosmoraes2303@gmail.com>
2019-07-21 13:06:11 -0300

    Configuration files (including sequelize)

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleare from './app/middlewares/auth';
 
+const upload = multer(multerConfig);
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 routes.use(authMiddleare);
 routes.put('/users', UserController.update);
 
